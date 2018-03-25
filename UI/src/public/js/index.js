02d25d7a06e6ac6e01451e9659e7450aece8dbce
Kevin Novak <kevinmnovak@yahoo.com>
2018-03-25 19:16:12 -0400

    Click events for color buttons

--- UI/src/public/js/index.js
+++ UI/src/public/js/index.js
@@ -1,11 +1,19 @@
 var ledButtons = document.getElementsByClassName('btn led');
+var colorButtons = document.getElementsByClassName('btn color');
 
 window.onload = () => {
     for (ledButton of ledButtons) {
         ledButton.addEventListener('click', ledClicked);
-    }    
+    }
+    for (colorButton of colorButtons) {
+        colorButton.addEventListener('click', colorClicked);
+    }
 };
 
 function ledClicked() {
+    alert(`${this.id} clicked!`);
+}
+
+function colorClicked() {
     alert(`${this.id} clicked!`);
 }
