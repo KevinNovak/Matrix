4dd644a7ea686bfa9b142192c275f748e98831b5
Kevin Novak <kevinmnovak@yahoo.com>
2018-03-25 19:46:46 -0400

    Basic colors

--- UI/src/public/js/index.js
+++ UI/src/public/js/index.js
@@ -24,7 +24,20 @@ function colorClicked() {
 }
 
 var colors = {
-    "color-1": "red",
-    "color-2": "green",
-    "color-3": "blue"
+    "color-1": "#b21f35",
+    "color-2": "#d82735",
+    "color-3": "#ff7435",
+    "color-4": "#ffa135",
+    "color-5": "#ffcb35",
+    "color-6": "#fff735",
+    "color-7": "#00753a",
+    "color-8": "#009e47",
+    "color-9": "#16dd36",
+    "color-10": "#0052a5",
+    "color-11": "#0079e7",
+    "color-12": "#06a9fc",
+    "color-13": "#681e7e",
+    "color-14": "#7d3cb5",
+    "color-15": "#bd7af6",
+    "color-16": "#dddddd"
 };
