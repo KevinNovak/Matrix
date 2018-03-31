1bf4908a0afb85f6788d1d3d7ad4f1ee4bd3cf54
Kevin Novak <kevinmnovak@yahoo.com>
2018-03-31 16:11:20 -0400

    Refactoring

--- UI/public/js/matrix.js
+++ UI/public/js/matrix.js
@@ -7,8 +7,8 @@ window.onload = () => {
     // Select elements
     ledButtons = document.getElementsByClassName('btn led');
     colorButtons = document.getElementsByClassName('btn color');
-    clearButton = document.getElementById('control-clear');
     setButton = document.getElementById('control-set');
+    clearButton = document.getElementById('control-clear');
 
     // Register events
     for (ledButton of ledButtons) {
@@ -17,21 +17,16 @@ window.onload = () => {
     for (colorButton of colorButtons) {
         colorButton.addEventListener('click', colorClicked);
     }
-    clearButton.addEventListener('click', clearAllClicked);
     setButton.addEventListener('click', setAllClicked);
+    clearButton.addEventListener('click', clearAllClicked);
 
     setActive(colorButtons[0]);
 };
 
-function ledClicked() {
-    console.log(`${this.id} clicked!`);
-    var color = activeColorButton.id;
-    setLedColor(this, color);
-    publishLed(this.id, color);
-}
-
+// ==============================================
+// Colors
+// ==============================================
 function colorClicked() {
-    console.log(`${this.id} clicked!`);
     setActive(this);
 }
 
@@ -43,31 +38,86 @@ function setActive(colorButton) {
     activeColorButton.classList.add('active');
 }
 
+// ==============================================
+// LEDs
+// ==============================================
+function ledClicked() {
+    var color = activeColorButton.id;
+    setLed(this, color);
+    publishLed(this.id, color);
+}
+
+function setLedById(ledId, color) {
+    var ledButton = document.getElementById(ledId);
+    setLed(ledButton, color);
+}
+
+function setLed(ledButton, color) {
+    removeLedColors(ledButton);
+    ledButton.classList.add(color);
+}
+
+function publishLed(ledId, color) {
+    var payload = JSON.stringify({
+        ledId,
+        color
+    });
+
+    client.publish('matrix/led', payload);
+}
+
+// ==============================================
+// Set all
+// ==============================================
 function setAllClicked() {
     var color = activeColorButton.id;
     setAll(color);
-    publishSetAll(color);
+    publishSet(color);
 }
 
+function setAll(color) {
+    for (ledButton of ledButtons) {
+        setLed(ledButton, color);
+    }
+}
+
+function publishSet(color) {
+    var payload = JSON.stringify({
+        color
+    });
+
+    client.publish('matrix/set', payload);
+}
+
+// ==============================================
+// Clear all
+// ==============================================
 function clearAllClicked() {
     clearAll();
     publishClear();
 }
 
 function clearAll() {
-    console.log('Clearing all leds');
     for (ledButton of ledButtons) {
-        setLedColor(ledButton, clearColor);
+        setLed(ledButton, clearColor);
     }
 }
 
-function setAll(color) {
-    console.log('Setting all leds');
-    for (ledButton of ledButtons) {
-        setLedColor(ledButton, color);
-    }
+function publishClear() {
+    client.publish('matrix/clear');
 }
 
+// ==============================================
+// Helpers
+// ==============================================
+function removeLedColors(ledButton) {
+    var regex = new RegExp('color\-[\\d]+', 'g');
+    ledButton.className = ledButton.className.replace(regex, '');
+}
+
+// ==============================================
+// Mosca
+// ==============================================
 // Connect to the MQTT Broker over WebSockets
 // The port here is the "http" port we specified on the MQTT Broker
 //var client = mqtt.connect('ws://192.168.0.27:80');
@@ -78,6 +128,14 @@ var client = mqtt.connect('ws://localhost:80');
 client.subscribe('matrix/led');
 client.subscribe('matrix/clear');
 client.subscribe('matrix/set');
+
+client.on('connect', () => {
+    console.log('Connected to MQTT Broker.');
+});
+
+client.on('close', () => {
+    console.log('Disconnected from MQTT Broker.');
+});
 
 // Message recieved
 client.on('message', (topic, payload) => {
@@ -100,50 +158,6 @@ client.on('message', (topic, payload) => {
             break;
     }
 });
-
-client.on('connect', () => {
-    console.log('Connected to MQTT Broker.');
-});
-
-client.on('close', () => {
-    console.log('Disconnected from MQTT Broker.');
-});
-
-function setLedById(ledId, color) {
-    var ledButton = document.getElementById(ledId);
-    setLedColor(ledButton, color);
-}
-
-function publishLed(ledId, color) {
-    var payload = JSON.stringify({
-        ledId,
-        color
-    });
-
-    client.publish('matrix/led', payload);
-}
-
-function publishClear() {
-    client.publish('matrix/clear');
-}
-
-function publishSetAll(color) {
-    var payload = JSON.stringify({
-        color
-    });
-
-    client.publish('matrix/set', payload);
-}
-
-function setLedColor(ledButton, color) {
-    removeLedColors(ledButton);
-    ledButton.classList.add(color);
-}
-
-function removeLedColors(ledButton) {
-    var regex = new RegExp('color\-[\\d]+', 'g');
-    ledButton.className = ledButton.className.replace(regex, '');
-}
 
 // // Close the connection
 // client.end();
