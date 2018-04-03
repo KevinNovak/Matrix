752221632d997598e64c4e06fbb6980a36944096
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-02 21:14:07 -0400

    Handlebars instead of plain HTML

--- UI/app.js
+++ UI/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const hbs = require('hbs');
 const path = require('path');
 const mosca = require('./mosca.js');
 const state = require('./state.js');
@@ -11,10 +12,17 @@ mosca.start();
 // App setup
 var app = express();
 
+// Handlebars
+app.set('view engine', 'hbs');
+
 // Static files
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/api/state', function (request, response) {
+app.get('/', (request, response) => {
+    response.render('index.hbs');
+});
+
+app.get('/api/state', (request, response) => {
     var body = state.leds;
     response.status(200);
     response.json(body);
