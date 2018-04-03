634be49cbfefcf2e7ad69e3661cda77df386e34c
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-02 21:37:25 -0400

    Pass in colors to render page

--- UI/app.js
+++ UI/app.js
@@ -20,7 +20,8 @@ app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (request, response) => {
     response.render('index.hbs', {
-        leds: state.leds
+        leds: state.leds,
+        colors: colors
     });
 });
 
@@ -34,3 +35,24 @@ app.get('/api/state', (request, response) => {
 var server = app.listen(httpPort, () => {
     console.log(`Server started on port ${httpPort}`);
 });
+
+var colors = [
+    "color-1",
+    "color-2",
+    "color-3",
+    "color-4",
+    "color-5",
+    "color-6",
+    "color-7",
+    "color-8",
+    "color-9",
+    "color-10",
+    "color-11",
+    "color-12",
+    "color-13",
+    "color-14",
+    "color-15",
+    "color-16",
+    "color-17",
+    "color-18"
+];
