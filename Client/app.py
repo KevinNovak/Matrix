e3ecf56b7a8d601d309c4bfff9b248760d172df7
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-07 17:07:50 -0400

    Refactoring

--- Client/app.py
+++ Client/app.py
@@ -1,7 +1,14 @@
 import paho.mqtt.client as mqtt
 
 
-# The callback for when the client receives a CONNACK response from the server.
+def start():
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect('kevinnovak.me', 1883, 60)
+    client.loop_forever()
+
+
 def on_connect(client, userdata, flags, rc):
     print('Connected to MQTT Broker.')
     subscribe()
@@ -11,7 +18,6 @@ def on_disconnect(client, userdata, rc):
     print('Disconnected from MQTT Broker.')
 
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print('  Topic: ' + msg.topic)
     print('  Payload: ' + str(msg.payload))
@@ -24,13 +30,4 @@ def subscribe():
 
 
 client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect('kevinnovak.me', 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+start()
