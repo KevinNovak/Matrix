e2d80e5decc97e441e10c534693b080284c4fd42
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-11 19:56:21 -0400

    Pre-emptively setting was a bad idea (refreshing causes issues)

--- UI/app.js
+++ UI/app.js
@@ -29,7 +29,7 @@ app.get('/', (request, response) => {
     response.render('index.hbs', {
         leds: state.leds,
         colors: colors,
-        online: state.online + 1
+        online: state.online
     });
 });
 
