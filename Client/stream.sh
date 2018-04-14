5c59ad5fdcb59eb5e2bf12fe39abfbe92e237896
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-13 21:34:36 -0400

    Command to multiple lines

--- Client/stream.sh
+++ Client/stream.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+
+# Kill any instances of UV4L
 pkill uv4l
-uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option '--port=9090' --server-option '--max-queued-connections=30' --server-option '--max-streams=25' --server-option '--max-threads=29'
+
+# Start streaming
+uv4l -nopreview \
+    --auto-video_nr \
+    --driver raspicam \
+    --encoding mjpeg \
+    --width 640 \
+    --height 480 \
+    --framerate 20 \
+    --server-option '--port=9090' \
+    --server-option '--max-queued-connections=30' \
+    --server-option '--max-streams=25' \
+    --server-option '--max-threads=29'
