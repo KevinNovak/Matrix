2651db105859fdfba1afbad979d661719deb003b
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-13 21:03:20 -0400

    Kill any running processes first

--- Client/stream.sh
+++ Client/stream.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+pkill uv4l
 uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option '--port=9090' --server-option '--max-queued-connections=30' --server-option '--max-streams=25' --server-option '--max-threads=29'
