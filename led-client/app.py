ead3d6c8d430ae57ac3389b89d22588de1e16adf
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-16 23:44:38 -0400

    Cleanup

--- led-client/app.py
+++ led-client/app.py
@@ -20,19 +20,14 @@ PAYLOAD_LEDS = 'leds'
 PAYLOAD_LED_ID = 'ledId'
 PAYLOAD_COLOR = 'color'
 
-# LED strip configuration:
-LED_COUNT = 16      # Number of LED pixels.
-LED_PIN = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-# True to invert the signal (when using NPN transistor level shift)
-LED_INVERT = False
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+# LED matrix configuration
+LED_COUNT = 16
+LED_PIN = 18
+LED_CHANNEL = 0  # Set to 1 for pins 13, 19, 41, 45 or 53
+LED_BRIGHTNESS = 255  # 0 to 255
 
-matrix = Matrix(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA,
-                LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+matrix = Matrix(LED_COUNT, LED_PIN, 800000, 10,
+                False, LED_BRIGHTNESS, LED_CHANNEL)
 client = mqtt.Client()
 
 
